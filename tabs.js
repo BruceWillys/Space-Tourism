a86977846dd326237d41d2962b0d89f204cd2a3f
BruceWillys <brucewillys24@gmail.com>
2022-09-22 12:22:04 +0100

    finished styling the destination pages and got started on adding the specific page functionality

---
+++ tabs.js
@@ -0,0 +1,14 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener("keydown", (event) => {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+    console.log(event);
+  // change the tabindex of the current tab to -1
+
+  // if the right key is pushed, move to the next tab on the right
+
+  // if the left key is pushed, move to the next tab on the left
+});
