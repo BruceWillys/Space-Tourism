dd2ffff6b80d2190bc1b9330da6d92e2440e5892
BruceWillys <brucewillys24@gmail.com>
2022-09-25 00:29:39 +0100

    finished styling the technology page and addded the finishing touches to all pages

--- tabs.js
+++ tabs.js
@@ -1,14 +1,105 @@
 const tabList = document.querySelector('[role="tablist"]');
-const tabs = document.querySelectorAll('[role="tab"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabList.addEventListener("keydown", (event) => {
+tabList.addEventListener("keydown", changeTabFocus);
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
   const keydownLeft = 37;
   const keydownRight = 39;
 
-    console.log(event);
-  // change the tabindex of the current tab to -1
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
 
-  // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
 
-  // if the left key is pushed, move to the next tab on the left
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+  }
+}
+
+const handleSizeCHange = () => {
+  const tButtons = document.querySelectorAll(".t-button");
+
+  let currentTButton = tButtons[0];
+
+  tButtons.forEach((tButton) => {
+    if (tButton.classList.contains("current-t-button")) {
+      currentTButton = tButton;
+    }
+  });
+
+  currentTButton.click();
+}
+
+window.addEventListener("load", () => {
+  handleSizeCHange()
+})
+
+window.addEventListener("resize", () => {
+  handleSizeCHange();
 });
+
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImage = targetTab.getAttribute("data-image");
+  const targetImageLg = targetTab.getAttribute("data-image-lg");
+
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+
+  
+  tabContainer
+    .querySelector('[aria-selected="true"]')?.setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
+  // mainContainer
+  //   .querySelectorAll('[role="tabpanel"]')
+  //   .forEach((panel) => panel.setAttribute("hidden", true));
+  hideContent(mainContainer, '[role="tabpanel"]');
+
+  // mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+  showContent(mainContainer, [`#${targetPanel}`]);
+
+  // mainContainer
+  //   .querySelectorAll("picture")
+  //   .forEach((picture) => picture.setAttribute("hidden", true));
+  hideContent(mainContainer, "picture");
+
+  // mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+  if (window.innerWidth >= 720) {
+    showContent(mainContainer, [`#${targetImageLg}`]);
+  } else {
+    showContent(mainContainer, [`#${targetImage}`]);
+  }
+}
+
+console.log(window.innerWidth);
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content)?.removeAttribute("hidden");
+  // document.querySelector(content).classList.toggle(".hiddden")
+  // parent.querySelector(content).classList.toggle(".hiddden");
+  // console.log(parent, content);
+}
